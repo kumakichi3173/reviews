9f6b56d1a5cac7210bae6b9d00c83f9ea050a465
kumakichi3173 <kumakichi3173@gmail.com>
2022-06-08 21:10:41 -0400

    uploaded

---
+++ 01_07.cpp
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main()
+{
+	int a,b;
+
+	printf("Type a positive value: ");
+	scanf("%d",&b);
+	for(a=0;a<b;a++)
+	{
+		printf("I must do this %d times\n",b);
+		fflush(stdout);
+		if(a==9)
+			break;
+	}
+
+	return(0);
+}
+
---
+++ linkedin.cpp
@@ -0,0 +1,7 @@
+#include<stdio.h>
+
+main (){
+    puts("This is my program");
+    puts("It does everything I ask");
+    return 0;
+}
