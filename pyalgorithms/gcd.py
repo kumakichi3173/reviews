931ba274f51098e13b262eb4d5e20c5c7121c6bb
Saori Shigehisa <kumakichi3173@gmail.com>
2022-06-10 15:10:20 -0400

    gdc.py was updated

--- pyalgorithms/gcd.py
+++ pyalgorithms/gcd.py
@@ -4,12 +4,13 @@
 def gcd(a, b):
     # pass
     while (b != 0):
-        t = a # t is a variable
-        a = b
-        b = t % b
+        t = a # t is a variable and used as a temp
+        a = b # set a equal to b
+        b = t % b # divide t (which is a) by b
 
     return a # if we find the GCD, then stop
 
-# try out thefunction with a few examples
+# try out the function with a few examples
+# change the values to get GCD
 print(gcd(60, 96)) # should be 12
 print(gcd(20, 8)) # should be 4
