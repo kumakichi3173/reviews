3a778550281faa799123f67893b8db73aadaf03b
kumakichi3173 <kumakichi3173@gmail.com>
2022-06-10 15:31:24 -0400

    linkedlist.py was updated

--- Ex_Files_Programming_Foundations_Algorithms/Ex_Files_Programming_Foundations_Algorithms/Exercise Files/2 Data Structures/LinkedList/linklist_start.py
+++ Ex_Files_Programming_Foundations_Algorithms/Ex_Files_Programming_Foundations_Algorithms/Exercise Files/2 Data Structures/LinkedList/linklist_start.py
@@ -30,33 +30,19 @@ class LinkedList(object):
         return self.count
 
     def insert(self, data):
+        # TODO: insert a new node
         new_node = Node(data)
-        new_node.set_next(self.head)
-        self.head = new_node
-        self.count += 1
 
     def find(self, val):
+        # TODO: find the first item with a given value
         item = self.head
-        while (item != None):
-            if item.get_data() == val:
-                return item
-            else:
-                item = item.get_next()
+
         return None
 
     def deleteAt(self, idx):
-        if idx > self.count:
+        # TODO: delete an item at given index
+        if idx > self.count-1:
             return
-        if self.head == None:
-            return
-        else:
-            tempIdx = 0
-            node = self.head
-            while tempIdx < idx-1:
-                node = node.get_next()
-                tempIdx += 1
-            node.set_next(node.get_next().get_next())
-            self.count -= 1
 
     def dump_list(self):
         tempnode = self.head
@@ -71,7 +57,6 @@ itemlist.insert(38)
 itemlist.insert(49)
 itemlist.insert(13)
 itemlist.insert(15)
-
 itemlist.dump_list()
 
 # exercise the list
@@ -80,7 +65,7 @@ print("Finding item: ", itemlist.find(13))
 print("Finding item: ", itemlist.find(78))
 
 # delete an item
-itemlist.deleteAt(3)
-print("Item count: ", itemlist.get_count())
-print("Finding item: ", itemlist.find(38))
-itemlist.dump_list()
+# itemlist.deleteAt(3)
+# print("Item count: ", itemlist.get_count())
+# print("Finding item: ", itemlist.find(38))
+# itemlist.dump_list()
